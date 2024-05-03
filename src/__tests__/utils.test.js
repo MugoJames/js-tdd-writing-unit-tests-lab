921f59a8039d5184b43dd408e3e4b8d079714c41
James <jamonjoroge816@gmail.com>
2024-05-03 14:50:44 +0300

    DONE

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,22 @@
 // Your tests here
+
+// Your tests here
+import { isPalindrome } from "../utils.js";
+
+describe("isPalindrome", () => {
+    it("determines if the string is the same reversed", () => {
+        const string = "racecar";
+
+        const palindrome = isPalindrome(string)
+
+        expect(palindrome).toBe(true)
+    });
+});
+
+it("handles uppercase and lowercase input", () => {
+    const string = "RaCeCaR";
+
+    const palindrome = isPalindrome(string);
+
+    expect(palindrome).toBe(true);
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
 // Your code here
+// Your code here
+export function isPalindrome(string) {
+    const arrayValues = string.split('');
+
+    const reverseArrayValues = arrayValues.reverse();
+
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        return true
+    } else {
+        return false
+    }
+}
